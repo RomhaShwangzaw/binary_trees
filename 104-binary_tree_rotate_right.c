491b73f11fa31acec068e8ff0c524dd8fcd18348
Romha Shwangzaw <romhas@gmail.com>
2022-12-14 16:37:13 +0300

    Bug Fix

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
 	binary_tree_t *pivot, *tmp;
 
-	if (tree == NULL || tree->right == NULL)
+	if (tree == NULL || tree->left == NULL)
 		return (NULL);
 
 	pivot = tree->left;
