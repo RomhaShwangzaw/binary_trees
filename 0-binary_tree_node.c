2a7ea886ffc1ae6079e57f116c1f8991a4ca0fff
Romha Shwangzaw <romhas@gmail.com>
2022-12-14 18:04:52 +0300

    Minor Fix

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -17,16 +17,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	node->n = value;
 	node->parent = parent;
-	node->left = node->right = NULL;
+	node->left = NULL;
+	node->right = NULL;
 
-	if (parent)
-	{
-		if (parent->left == NULL)
-			parent->left = node;
-		else if (parent->right == NULL)
-			parent->right = node;
-		else
-			return (NULL);
-	}
 	return (node);
 }
