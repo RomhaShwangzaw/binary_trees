32d2c6333e2425c2e038014eaf0aef8b0e55932d
Romha Shwangzaw <romhas@gmail.com>
2022-12-14 16:28:50 +0300

    binary_tree_rotate_left function added

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -8,11 +8,27 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
+	binary_tree_t *pivot, *tmp;
+
 	if (tree == NULL || tree->right == NULL)
 		return (NULL);
 
-	tree->parent = tree->right;
-	tree->right = tree->right->left;
-	tree->right->left = tree;
-	return (tree->right);
+	pivot = tree->right;
+	tmp = pivot->left;
+	pivot->left = tree;
+	tree->right = tmp;
+	if (tmp != NULL)
+		tmp->parent = tree;
+	tmp = tree->parent;
+	tree->parent = pivot;
+	pivot->parent = tmp;
+	if (tmp != NULL)
+	{
+		if (tmp->left == tree)
+			tmp->left = pivot;
+		else
+			tmp->right = pivot;
+	}
+
+	return (pivot);
 }
