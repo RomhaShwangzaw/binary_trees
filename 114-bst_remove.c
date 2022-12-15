288589e693fad5dc568e643a451747914a21d2f5
Romha Shwangzaw <romhas@gmail.com>
2022-12-15 13:19:41 +0300

    Bug Fix

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,5 +1,9 @@
 #include "binary_trees.h"
 
+bst_t *min_value_node(bst_t *tree);
+void bst_delete(bst_t *node, bst_t *successor);
+bst_t *bst_remove(bst_t *root, int value);
+
 /**
  * min_value_node - finds the node with minimum key value found in that tree.
  * @tree: a pointer to the root node of the tree where we will search for
@@ -12,6 +16,24 @@ bst_t *min_value_node(bst_t *tree)
 	while (tree && tree->left)
 		tree = tree->left;
 	return (tree);
+}
+
+/**
+ * bst_delete - deletes a node from a BST.
+ * @node: a pointer to the node to be removed.
+ * @successor: a pointer to the inorder successor node to replace @root.
+ */
+void bst_delete(bst_t *node, bst_t *successor)
+{
+	bst_t *parent;
+
+	parent = node->parent;
+	if (parent && parent->left == node)
+		parent->left = successor;
+	else if (parent)
+		parent->right = successor;
+	if (successor)
+		successor->parent = parent;
 }
 
 /**
@@ -43,12 +65,14 @@ bst_t *bst_remove(bst_t *root, int value)
 		if (root->left == NULL)
 		{
 			tmp = root->right;
+			bst_delete(root, tmp);
 			free(root);
 			return (tmp);
 		}
 		else if (root->right == NULL)
 		{
 			tmp = root->left;
+			bst_delete(root, tmp);
 			free(root);
 			return (tmp);
 		}
